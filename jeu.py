3ec92e7be9c7fac9e9d3dc5739688048853ba158
33674 <lloisjourdan@gmail.com>
2021-01-10 17:27:55 +0100

    test

--- jeu.py
+++ jeu.py
@@ -5,10 +5,16 @@ class Jeu:
     def __init__(self, ecran):
         self.ecran = ecran
 
+    def boucle_du_jeu(self):
+        print("tralala")
 
-    def choisir_qui_commence:
+
+    def choisir_qui_commence(self):
         alea = random.randint(1,2)
         if alea == 1:
             print("machine commence")
         else:
             print("joueur commence")
+
+    def affichage_plateau(self):
+        print("trululu")
--- main.py
+++ main.py
@@ -1,22 +1,42 @@
 import pygame
 import traceback
+from jeu import Jeu
 
 pygame.init()
+
+jeu_en_cours = True
 
 #génération fenetre
 pygame.display.set_caption("morpion")
 ecran = pygame.display.set_mode((1080,700))
 
 #import arrière plan
-ArrierePlan = pygame.image.load('image/bg.jpg')
+ArrierePlan = pygame.image.load('bg.jpg')
 
 #importé le jeu
 jeu = Jeu(ecran)
 
 def jeu_boucle():
+    global jeu_en_cours
     try:
+        # appliquer arrière plan
+        ecran.blit(ArrierePlan, (0, -200))
 
+        jeu.boucle_du_jeu()
+
+        # mettre a jour l'écrant
+        pygame.display.flip()
+
+        # si il ferme la fenetre
+        for event in pygame.event.get():
+            # fermeture fenetre
+            if event.type == pygame.QUIT:
+                jeu_en_cours = False
+                pygame.quit()
 
     except Exception as e:
         texte = str(traceback.extract_tb(e.__traceback__, None))
         print(texte)
+
+while jeu_en_cours:
+    jeu_boucle()
