2825245dc2fd83c44a763bc7ee34e093f89eecba
33674 <lloisjourdan@gmail.com>
2021-01-10 12:45:54 +0100

    test

--- jeu.py
+++ jeu.py
@@ -1,5 +1,14 @@
 import pygame
+import random
 
 class Jeu:
     def __init__(self, ecran):
         self.ecran = ecran
+
+
+    def choisir_qui_commence:
+        alea = random.randint(1,2)
+        if alea == 1:
+            print("machine commence")
+        else:
+            print("joueur commence")
